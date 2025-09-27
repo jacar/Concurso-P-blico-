513c20b12eea29c5f0954c8acbb1390129450bdb
Armando José Ovalle <ovalle_938@hotmail.com>
2025-09-27 18:39:50 -0500

    fix: update TypeScript and Vite config for JSON imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -26,7 +29,8 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, './src'),
           '~': path.resolve(__dirname, './node_modules')
-        }
+        },
+        extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
       },
       css: {
         preprocessorOptions: {
